ed794d4a75484236c09424f32362413cd0af01bb
YouCanBeGod <dallasta.amos@gmail.com>
2022-11-15 23:49:16 -0300

    Criação de Métodos

--- OrientacaoObjeto/src/Carro.java
+++ OrientacaoObjeto/src/Carro.java
@@ -2,6 +2,44 @@ public class Carro {
 
     String modelo;
     String cor;
-    double CapacidadeTanque;
+    double capacidadeTanque;
+
+    Carro(String modelo, String cor, double capacidadeTanque) {
+        this.modelo = modelo;
+        this.cor = cor;
+        this.capacidadeTanque = capacidadeTanque;
+    }
+
+    void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    String getModelo() {
+        return modelo;
+    }
+
+    void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    String getCor() {
+        return cor;
+    }
+
+    void setCapacidadeTanque(double capacidadeTanque) {
+        this.capacidadeTanque = capacidadeTanque;
+    }
+
+    double getCapacidadeTanque() {
+        return capacidadeTanque;
+    }
+
+    public double valorEncherTanque(double valorGasolina) {
+        return capacidadeTanque * valorGasolina;
+    }
+
+    public double valorEncherTanque(double valorGasolina, double desconto) {
+        return (capacidadeTanque * valorGasolina) * desconto / 100;
+    }
 
 }
